7d4efe6b0d94be070d5d83da6e32d290e0667628
Azhar730 <azharmahmud730@gmail.com>
2024-10-12 19:04:08 +0600

    final commit

--- index.js
+++ index.js
@@ -5,30 +5,30 @@ require('dotenv').config()
 const port = process.env.PORT || 5000
 const app = express()
 const corsOptions = {
-    origin: ['http://localhost:5173','http://localhost:5174'],
+    origin: ['http://localhost:5173', 'http://localhost:5174'],
     credentials: true,
     optionSuccessStatus: 200
 }
 app.use(cors(corsOptions))
 app.use(express.json())
-const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.apuyeda.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.apuyeda.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const client = new MongoClient(uri, {
     serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
     }
-  });
-  async function run() {
+});
+async function run() {
     try {
 
-      await client.db("admin").command({ ping: 1 });
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
     }
-  }
-  run().catch(console.dir);
-app.get('/', (req,res)=>{
+}
+run().catch(console.dir);
+app.get('/', (req, res) => {
     res.send('Hello from SoloSphere2 Server')
 })
-app.listen(port,()=>console.log(`Server Running on Port ${port}`))
+app.listen(port, () => console.log(`Server Running on Port ${port}`))
